f0f162347b9c873984002de4c28ac4285507a2d6
engsametsahin <43150069+engsametsahin@users.noreply.github.com>
2021-09-26 14:41:20 +0300

    Add files via upload

---
+++ if-else-if/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time>= 6 && time <11)
+            {
+            Console.WriteLine("Günaydın");
+            }
+            else if (time <= 18)
+            {
+            Console.WriteLine("İyi Günler");
+            }
+            else
+            {
+            Console.WriteLine("İyi Akşamlar");
+            }
+
+        string sonuc = time <= 18 ? "İyi Günler" : "İyi Geceler";
+        Console.WriteLine(sonuc);
+        }
+    }
+}
