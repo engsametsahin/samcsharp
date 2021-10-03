f7d562fe07ddfd69cf0d9309f6b33c6865972b2c
engsametsahin <43150069+engsametsahin@users.noreply.github.com>
2021-10-03 23:31:36 +0300

    Add files via upload

---
+++ diziler-array-sinifi-metodlari/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace diziler_array_sinifi_metodlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int [] sayidizisi = {23,12,4,86,72,3,11,17};
+            foreach(var item in sayidizisi)
+                {
+                Console.WriteLine(item);
+                }
+            
+            //Sort
+            Array.Sort(sayidizisi);
+            foreach(var item in sayidizisi)
+                {
+                Console.WriteLine(item);
+                }
+
+            //Clear
+            Array.Clear(sayidizisi,2,2);
+            foreach(var item in sayidizisi)
+                {
+                Console.WriteLine(item);
+                }
+
+            //Reverse
+            Array.Reverse(sayidizisi);
+            foreach(var item in sayidizisi)
+                {
+                Console.WriteLine(item);
+                }
+            
+            //IndexOf
+                Console.WriteLine(Array.IndexOf(sayidizisi,23));
+
+            //Resize
+            Array.Resize <int> (ref sayidizisi,9);
+            sayidizisi[8] = 99;
+            foreach(var item in sayidizisi)
+                {
+                Console.WriteLine(item);
+                }            
+
+        }
+    }
+}
